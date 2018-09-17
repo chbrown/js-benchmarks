62456838ad4c669246a154091411e9078a257439
Christopher Brown <io@henrian.com>
2018-09-17 16:05:57 -0500

    Add 'deep' example data

--- _data.ts
+++ _data.ts
@@ -39,3 +39,126 @@ export const arrays: any[][] = [
     91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
   ],
 ]
+
+export const deep: object = {
+  list: {
+    OoVwOM: {
+      id: 'OoVwOM',
+      parent: 'pTlmbh',
+      name: 'New node',
+      created_at: 1384289621,
+    },
+    aHxe8k: {
+      id: 'aHxe8k',
+      parent: 'Fhs2hL',
+      name: 'hjkhjkhjk',
+      created_at: 1384354593,
+    },
+    Fhs2hL: {
+      id: 'Fhs2hL',
+      parent: 'root',
+      name: 'test',
+      created_at: 1383403881,
+    },
+    HYPSgv: {
+      id: 'HYPSgv',
+      parent: 'OoVwOM',
+      name: 'New node',
+      created_at: 1384342657,
+    },
+    osFIMf: {
+      id: 'osFIMf',
+      parent: 'root',
+      name: 'New node',
+      created_at: 1384354584,
+    },
+    PsovXE: {
+      id: 'PsovXE',
+      parent: 'root',
+      name: 'hjkhjkhjk',
+      created_at: 1384354589,
+    },
+    pTlmbh: {
+      id: 'pTlmbh',
+      parent: 'Fhs2hL',
+      name: 'New node',
+      created_at: 1384289277,
+    },
+    RbXhdJ: {
+      id: 'RbXhdJ',
+      parent: 'root',
+      name: 'empty',
+      created_at: 1384359806,
+    },
+  },
+  maps: {
+    parent: {
+      pTlmbh: {
+        OoVwOM: {
+          id: 'OoVwOM',
+          parent: 'pTlmbh',
+          name: 'New node',
+          created_at: 1384289621,
+        },
+      },
+      Fhs2hL: {
+        aHxe8k: {
+          id: 'aHxe8k',
+          parent: 'Fhs2hL',
+          name: 'hjkhjkhjk',
+          created_at: 1384354593,
+        },
+        pTlmbh: {
+          id: 'pTlmbh',
+          parent: 'Fhs2hL',
+          name: 'New node',
+          created_at: 1384289277,
+        },
+      },
+      root: {
+        Fhs2hL: {
+          id: 'Fhs2hL',
+          parent: 'root',
+          name: 'test',
+          created_at: 1383403881,
+        },
+        osFIMf: {
+          id: 'osFIMf',
+          parent: 'root',
+          name: 'New node',
+          created_at: 1384354584,
+        },
+        PsovXE: {
+          id: 'PsovXE',
+          parent: 'root',
+          name: 'hjkhjkhjk',
+          created_at: 1384354589,
+        },
+        RbXhdJ: {
+          id: 'RbXhdJ',
+          parent: 'root',
+          name: 'empty',
+          created_at: 1384359806,
+        },
+      },
+      OoVwOM: {
+        HYPSgv: {
+          id: 'HYPSgv',
+          parent: 'OoVwOM',
+          name: 'New node',
+          created_at: 1384342657,
+        },
+      },
+    },
+    path: [
+      ['Fhs2hL'],
+      ['Fhs2hL', 'aHxe8k'],
+      ['Fhs2hL', 'pTlmbh'],
+      ['Fhs2hL', 'pTlmbh', 'OoVwOM'],
+      ['Fhs2hL', 'pTlmbh', 'OoVwOM', 'HYPSgv'],
+      ['osFIMf'],
+      ['PsovXE'],
+      ['RbXhdJ'],
+    ],
+  },
+}
